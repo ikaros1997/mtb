a6d48b3202fc2242023548d92de999c138d3d3a0
wl <3279953576@qq.com>
2022-04-24 20:57:29 +0800

    111

--- mtb/urls.py
+++ mtb/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 # from django.contrib import admin
 from django.urls import path, include
-# from apps import base
+from apps import base
 
 urlpatterns = [
     # path('admin/', admin.site.urls),
